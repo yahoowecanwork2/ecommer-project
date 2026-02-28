ba9e144c206fb191b01df81605eaa7e0a0c5cd0d
Sanjeev Kumar <109661686+softlabxsanjeev1@users.noreply.github.com>
2026-02-28 11:33:46 +0530

    ans wislist file added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { userSlice } from "./userSlice";
+import { cartSlice } from "./cartSlice";
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  cart: cartSlice.reducer,
 });
 
 const store = configureStore({
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -25,5 +25,4 @@ adminRoutes.put("/send-mail",isAuthenticated, sendEmailToUser);
                  
 
 
-
 export default adminRoutes;
