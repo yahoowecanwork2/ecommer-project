f1a2ad990b977a10bbf34c1b9e22a140210900c3
Neha Yadav <ny663922@gmail.com>
2026-02-28 11:44:33 +0530

    header

--- frontend/src/pages/common/Headerhome.jsx
+++ frontend/src/pages/common/Headerhome.jsx
@@ -6,6 +6,7 @@ import {
   IoPerson,
 } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { IoHeartOutline } from "react-icons/io5";
 
 const HeaderHome = () => {
   return (
@@ -15,29 +16,17 @@ const HeaderHome = () => {
           <IoMenu className="text-2xl cursor-pointer lg:hidden" />
 
           <Link to="/">
-            <h1 className="text-xl font-bold">PharmaClosing</h1>
+            <h1 className="text-xl font-bold">Ecommerce</h1>
           </Link>
-        </div>
-
-        {/* Center: Search */}
-        <div className="hidden lg:flex items-center">
-          <div className="relative">
-            <IoSearchOutline className="absolute left-2 top-2.5 text-gray-500" />
-            <input
-              type="text"
-              placeholder="Search medicines"
-              className="pl-8 pr-3 h-9 rounded-l-sm text-black"
-            />
-          </div>
-          <button className="bg-[#3A00E6] h-9 px-4 rounded-r-sm">Search</button>
         </div>
 
         <div className="flex items-center gap-6">
           <Link to="/dashboard">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/product">Product</Link>
 
-          <Link to="/whishlist">
-            <IoCartOutline className="text-2xl" />
+          <Link to="/wishlist">
+            <IoHeartOutline className="text-2xl" />
           </Link>
           <Link to="/cart">
             <IoCartOutline className="text-2xl" />
