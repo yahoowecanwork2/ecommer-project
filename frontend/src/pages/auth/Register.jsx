dd4a7791a90503f2a9784ac16afb1d1741e5211c
Lee Home Packers And Movers <leecompany848@gmail.com>
2026-03-17 14:13:29 +0530

    Update Register.jsx

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -40,7 +40,7 @@ const Register = () => {
     }
   }, []);
 
-  // ---------------- Send OTP ----------------
+  // ---------------- Send OTP ------------------------
   const sendOtp = async (type) => {
     try {
       if (!window.recaptchaVerifier) {
