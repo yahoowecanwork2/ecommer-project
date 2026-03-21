0865a7423d662a2949bb085ffc0b0890c802fea8
Lee Home Packers And Movers <leecompany848@gmail.com>
2026-03-21 16:36:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import app from './app/app.js';
 
 
 
-// // create the server -----------------------------------
+// // create the server --------------------------------------------
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app)
 server.listen(PORT, console.log(`Server is up and running on port ${PORT}`))
