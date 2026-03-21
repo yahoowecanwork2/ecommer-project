90e7a5b4737778cee2d767cd6153cea689968c23
Lee Home Packers And Movers <leecompany848@gmail.com>
2026-03-21 17:05:02 +0530

    revert changes

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -103,24 +103,24 @@ export const updateCategory = async (req, res) => {
   }
 };
 
-// export const deleteCategory = async (req, res) => {
-//   try {
-//     const deletedCategory = await Category.findByIdAndDelete(
-//       req.params.categoryId
-//     );
+export const deleteCategory = async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(
+      req.params.categoryId
+    );
 
-//     if (!deletedCategory) {
-//       return res.status(404).json({ message: "Category not found" });
-//     }
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
 
-//     res.status(200).json({
-//       message: "Category deleted successfully",
-//       data: deletedCategory,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "Error deleting category",
-//       error: error.message,
-//     });
-//   }
-// };
+    res.status(200).json({
+      message: "Category deleted successfully",
+      data: deletedCategory,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting category",
+      error: error.message,
+    });
+  }
+};
