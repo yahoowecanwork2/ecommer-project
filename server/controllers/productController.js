7732cbcfebfb43c2b39d3f81eb464a2e5b01290f
Lee Home Packers And Movers <leecompany848@gmail.com>
2026-04-03 17:25:08 +0530

    the issue of not uploading images was large data
    Fixed by increasing the transmission window at the nginx config of ecomserver.arcoders.com.conf

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -437,9 +437,6 @@ export const createProducts = async (req, res) => {
       imageGroups,
     } = req.body;
 
-    console.log("BODY:", req.body);
-    console.log("FILES:", req.files);
-
     const parsedVariants = JSON.parse(variants);
 
     if (!parsedVariants || parsedVariants.length === 0) {
